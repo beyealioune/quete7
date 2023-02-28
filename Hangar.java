40017fc35dcfb8f0884a004dea0a533fe034a897
BEYE ALIOUNE <alioune.beye@gmail.com>
2023-02-28 17:38:22 +0100

    fin

---
+++ Hangar.java
@@ -0,0 +1,15 @@
+public class Hangar {
+
+    public static void main(String[] args) {
+
+
+        Car mercedes = new Car("mercedes");
+        Boat bateau = new Boat("yatch");
+
+        System.out.println(mercedes.doStuff());
+        System.out.println(bateau.doStuff());
+    }
+
+
+
+}
